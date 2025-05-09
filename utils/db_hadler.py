8993a370d599269a18325e3a3ca967b7e10824ca
yulmi <avs268@naver.com>
2025-05-09 17:17:43 +0900

    try to merge with add-db

--- utils/db_hadler.py
+++ utils/db_hadler.py
@@ -64,15 +64,38 @@ def delete_schema_by_semantic_id(semantic_id: str) -> bool:
         print(f"Error occurred: {e}")
 
 # Schema 조회
-# def retrive_schemas():
-#     client = get_db_client()
-#     try:
-#         client.admin.command("ping")
-#         collection = client.aas.aas_schema
-#         documents = collection.find({}, {"submodel_id": 1, "_id": 0})
+def retrieve_schemas():
+    client = get_db_client()
+    try:
+        client.admin.command("ping")
+        collection = client.aas.aas_schema
+        documents = collection.find({}, {"submodel_id": 1, "_id": 0})
 
-#         submodel_ids = [doc["submodel_id"] for doc in documents]
-#         return submodel_ids
+        submodel_ids = [doc["submodel_id"] for doc in documents]
+        return submodel_ids
 
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=f"DB check schema error: {}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"DB check schema error: {e}")
+
+
+def search_schema_in_all_fields(value: str):
+    client = get_db_client()
+    try:
+        client.admin.command("ping")
+        collection = client.aas.aas_schema
+
+        query = {
+            "$or": [
+                {"submodel_id": value},
+                {"uploaded_by": value},
+                {"version": value},
+                {"revision": value}
+            ]
+        }
+
+        documents = collection.find(query)
+        return list(documents)
+
+    except Exception as e:
+        print(f"[DB Error] {e}")
+        return []
