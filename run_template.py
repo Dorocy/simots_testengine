60e14cb9e2f6d3f3dd279cbe5b7266d1b2548867
yulmi <avs268@naver.com>
2025-10-30 15:06:51 +0900

    10-30 constraint 작업중

--- run_template.py
+++ run_template.py
@@ -1,29 +1,69 @@
-import os
-import sys
+import os, sys, json
 
 from aas_test_engines.test_cases.v3_0.parse import check_constraints, _parse_and_check
 from aas_test_engines.test_cases.v3_0.__init__ import json_to_obj
 from aas_test_engines.result import AasTestResult
 
 
-def run_constraint_check(file_path: str, parsed_result: dict = None) -> dict:
+# def run_constraint_check(file_path: str, parsed_result: dict = None) -> dict:
+#     print("constraint check 시작")
+
+#     try:
+#         result_root, env = json_to_obj(parsed_result, "Environment")
+#         print('env: ', env)
+
+#         if env is None:
+#             print("환경 파싱 실패")
+#             return result_root.dump()
+
+#         check_constraints(env, result_root)
+
+#         print("constraint check 완료")
+#         print(result_root.dump())
+#         return result_root.dump()
+
+#     except Exception as e:
+#         print(f"[Constraint Check Error] {e}")
+#         return {"status": "Constraint Check Failed", "error": str(e)}
+
+# def run_constraint_check(file_path: str) -> str:
+#     print("constraint check 시작")
+
+#     try:
+#         json_data = json.loads(file_content.decode("utf-8"))
+
+#         result_root, env = json_to_obj(parsed_json, "Environment")
+#         # print('env: ', env)
+#         print('parsed json: ', parsed_json)
+
+#         if env is None:
+#             return f"[Constraint Check Error] Failed to parse environment for {file_path}"
+
+#         constraint_result = AasTestResult("Forced constraint check")
+#         check_constraints(env, constraint_result)
+
+#         return constraint_result.dump()
+
+#     except Exception as e:
+#         return f"[Forced constraint check error] {e}"
+
+def run_constraint_check(json_data: dict):
     print("constraint check 시작")
 
     try:
-        result_root, env = json_to_obj(parsed_result, "Environment")
-        print('env: ', env)
-
+        # 1. JSON → Environment 객체
+        result_root, env = json_to_obj(json_data, "Environment")
         if env is None:
-            print("환경 파싱 실패")
+            print("Environment 파싱 실패")
             return result_root.dump()
 
-        # ✅ Environment 전체에 대해 constraint 검사
+        # 2. Environment 전체에 대해 constraint 검사
         check_constraints(env, result_root)
 
-        print("constraint check 완료")
-        print(result_root.dump())
         return result_root.dump()
 
     except Exception as e:
-        print(f"[Constraint Check Error] {e}")
-        return {"status": "Constraint Check Failed", "error": str(e)}
+        print(f"[Forced Constraint Check Error] {e}")
+        error_result = AasTestResult("Forced Constraint Check Failed")
+        error_result.append(AasTestResult(f"Exception: {e}"))
+        return error_result.dump()
--- test_engine_run.py
+++ test_engine_run.py
@@ -1,9 +1,78 @@
-import json, re, os, sys
+import json, io, re, os, sys
 import subprocess
 from utils.response_handler import ErrorCode, error_response, success_response
 from run_template import run_constraint_check
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from aas_core3.types import Environment
+
+
+# def run_test_engine(file_path: str, file_ext: str, is_template: bool = False) -> dict:
+#     command = build_command(file_path, file_ext)
+#     env = os.environ.copy()
+
+#     env["PYTHONIOENCODING"] = "utf-8"
+#     try:
+#         result = subprocess.run(command, capture_output=True, env=env)
+
+#         if not result:
+#             return error_response(500, ErrorCode.TEST_ENGINE_NO_OUTPUT)
+
+#         if result.stdout:
+#             result.stdout = result.stdout.decode('utf-8-sig', errors='replace')
+#             return parse_engine_output(result.stdout, is_stdout=True, is_template=is_template)
+
+#         if result.stderr:
+#             result.stderr = result.stderr.decode('utf-8-sig', errors='replace')
+#             return parse_engine_output(result.stderr, is_stdout=False, is_template=is_template)
+
+#         return error_response(500, ErrorCode.TEST_ENGINE_NO_OUTPUT)
+
+#     except json.JSONDecodeError as e:
+#         return error_response(500, ErrorCode.INVALID_JSON_FORMAT, {str(e)})
+
+#     except Exception:
+#         return error_response(500, ErrorCode.INTERNAL_SERVER_ERROR)
+
+
+# async def run_test_engine(file, file_path: str, file_ext: str, is_template: bool = False) -> dict:
+#     command = build_command(file_path, file_ext)
+#     env = os.environ.copy()
+#     env["PYTHONIOENCODING"] = "utf-8"
+
+#     try:
+#         result = subprocess.run(command, capture_output=True, env=env)
+
+#         if not result:
+#             return error_response(500, ErrorCode.TEST_ENGINE_NO_OUTPUT)
+
+#         raw_bytes = result.stdout if result.stdout else result.stderr
+#         is_stdout = bool(result.stdout)
+#         raw_output = raw_bytes.decode('utf-8-sig', errors='replace')
+
+#         if is_template:
+#             with open(file_path, "r", encoding="utf-8") as f:
+#                 file_content = json.load(f)
+#                 print('aaaaaaaa: ', file_content)
+#             # try:
+#             #     json_data = json.loads(file_content.decode("utf-8"))
+#             # except Exception:
+#             #     return ErrorCode.INVALID_JSON_FORMAT
+
+#             buffer = io.StringIO()
+#             sys.stdout = buffer
+#             constraint_output = run_constraint_check(json_data)
+
+#             raw_output = raw_output + "\n" + constraint_output
+
+#         final_parsed = parse_engine_output(raw_output, is_stdout=is_stdout, is_template=is_template)
+
+#         return final_parsed
+
+#     except json.JSONDecodeError as e:
+#         return error_response(500, ErrorCode.INVALID_JSON_FORMAT, {str(e)})
+#     except Exception as e:
+#         return error_response(500, ErrorCode.INTERNAL_SERVER_ERROR, {str(e)})
 
 
 def run_test_engine(file_path: str, file_ext: str, is_template: bool = False) -> dict:
@@ -17,15 +86,25 @@ def run_test_engine(file_path: str, file_ext: str, is_template: bool = False) ->
         if not result:
             return error_response(500, ErrorCode.TEST_ENGINE_NO_OUTPUT)
 
+        output_str = None
         if result.stdout:
-            result.stdout = result.stdout.decode('utf-8-sig', errors='replace')
-            return parse_engine_output(result.stdout, is_stdout=True, is_template=is_template)
+            output_str = result.stdout.decode('utf-8-sig', errors='replace')
+        elif result.stderr:
+            output_str = result.stderr.decode('utf-8-sig', errors='replace')
+        else:
+            return error_response(500, ErrorCode.TEST_ENGINE_NO_OUTPUT)
 
-        if result.stderr:
-            result.stderr = result.stderr.decode('utf-8-sig', errors='replace')
-            return parse_engine_output(result.stderr, is_stdout=False, is_template=is_template)
+        if is_template:
+            try:
+                json_data = parsed_result.get("json_data")
+                if json_data:
+                    forced_result = run_constraint_check(json_data)
+                    parsed_result["forced_constraint_check"] = forced_result
+            except Exception as e:
+                parsed_result["forced_constraint_check"] = {"error": str(e)}
 
-        return error_response(500, ErrorCode.TEST_ENGINE_NO_OUTPUT)
+        parsed_result = parse_engine_output(output_str, is_stdout=bool, is_template=is_template)
+        return parsed_result
 
     except json.JSONDecodeError as e:
         return error_response(500, ErrorCode.INVALID_JSON_FORMAT, {str(e)})
@@ -33,50 +112,9 @@ def run_test_engine(file_path: str, file_ext: str, is_template: bool = False) ->
     except Exception:
         return error_response(500, ErrorCode.INTERNAL_SERVER_ERROR)
 
-# def run_test_engine(file_path: str, file_ext: str, is_template: bool = False) -> dict:
-#     command = build_command(file_path, file_ext)
-#     env = os.environ.copy()
-#     env["PYTHONIOENCODING"] = "utf-8"
-
-#     try:
-#         result = subprocess.run(command, capture_output=True, env=env)
-
-#         if not result:
-#             return error_response(500, ErrorCode.TEST_ENGINE_NO_OUTPUT)
-
-#         parsed_result = None
-
-#         # 기존 stdout/parse_engine_output 처리
-#         if result.stdout:
-#             result.stdout = result.stdout.decode('utf-8-sig', errors='replace')
-#             parsed_result = parse_engine_output(result.stdout, is_stdout=True, is_template=is_template)
-
-#         elif result.stderr:
-#             result.stderr = result.stderr.decode('utf-8-sig', errors='replace')
-#             parsed_result = parse_engine_output(result.stderr, is_stdout=False, is_template=is_template)
-
-#         # if isinstance(parsed_result, JSONResponse):
-#         #     parsed_result = parsed_result.body.decode("utf-8")
-#         #     parsed_result = json.loads(parsed_result)
-
-#         # is_template=True면 강제로 constraint 검사
-#         # if is_template:
-#         #     print('is template까지 옴')
-#         #     constraint_result = run_constraint_check(file_path, parsed_result)
-#         #     print(parsed_result)
-#         #     parsed_result = parse_engine_output(constraint_result, is_template=is_template)
-
-#         return parsed_result
-
-#     except json.JSONDecodeError as e:
-#         return error_response(500, ErrorCode.INVALID_JSON_FORMAT, {str(e)})
-
-#     except Exception:
-#         return error_response(500, ErrorCode.INTERNAL_SERVER_ERROR)
-
 
 def build_command(file_path: str, file_ext: str) -> list:
-    base_command = ["aas_test_engines", "check_file", file_path]
+    base_command = ["python", "-m", "aas_test_engines", "check_file", file_path]
 
     if file_ext == ".json":
         base_command += ["--format", "json"]
@@ -90,17 +128,17 @@ ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
 
 def parse_engine_output(output: str, is_stdout: bool = True, is_template: bool = False) -> dict:
-    if output.startswith('\u001b[92mCheck'):  # 초록색
-        verification_status = "success"
-    elif output.startswith('\u001b[91mCheck'):  # 빨간색
-        verification_status = "failed"
+    if output.startswith('\u001b[92mCheck'):  # 초록색, 노란색
+        verification_status = 'success'
+    elif output.startswith('\u001b[91mCheck') or output.startswith('\u001b[93mCheck'):  # 빨간색
+        verification_status = 'failed'
     else:
         verification_status = None
 
     try:
         lines = output.strip().splitlines()
 
-        check_msg = []
+        needless_msg = []
         asset_info_msgs = []
         submodel_msgs = []
         concept_description_msgs = []
@@ -112,7 +150,6 @@ def parse_engine_output(output: str, is_stdout: bool = True, is_template: bool =
 
             if clean_line.startswith('f"Constraint violated:'):
                 constraint_msgs.append(clean_line)
-                # print(clean_line)
                 verification_status = 'failed'
                 continue
 
@@ -123,7 +160,9 @@ def parse_engine_output(output: str, is_stdout: bool = True, is_template: bool =
                 continue
 
             if is_stdout is True:
-                if clean_line.startswith('Constraint '):
+                if clean_line.startswith('Constraint AASd-120') or clean_line.startswith('Check') or clean_line.startswith('Skipped') or clean_line.startswith('Template:'):
+                    needless_msg.append(clean_line)
+                elif clean_line.startswith('Constraint '):
                     constraint_msgs.append(clean_line)
                 elif "@ /assetAdministrationShells" in clean_line:
                     asset_info_msgs.append(clean_line)
@@ -131,8 +170,6 @@ def parse_engine_output(output: str, is_stdout: bool = True, is_template: bool =
                     submodel_msgs.append(clean_line)
                 elif "@ /conceptDescriptions" in clean_line:
                     concept_description_msgs.append(clean_line)
-                elif clean_line.startswith('Check') or clean_line.startswith('Skipped') or clean_line.startswith('Template:') or clean_line.startwith('Constraint AASd-120 violated: element 0 must not have an idShort'):
-                    check_msg.append(clean_line)
                 else:
                     etc_msgs.append(clean_line)
 
@@ -159,7 +196,7 @@ def parse_engine_output(output: str, is_stdout: bool = True, is_template: bool =
             }
         }
 
-        if is_template:
+        if (is_template or is_stdout):
             total_msgs = (len(asset_info_msgs)
                           + len(submodel_msgs)
                           + len(concept_description_msgs)
@@ -172,10 +209,10 @@ def parse_engine_output(output: str, is_stdout: bool = True, is_template: bool =
 
         if verification_status == 'success':
             return success_response("Model API", "success", "PERFECT")
-        elif verification_status == "failed":
+        elif verification_status == 'failed':
             return success_response("Model API", "failed", verification_message)
         else:
-            return success_response("Model API", "warning", verification_message)
+            return error_response("400", "Unknown Error", verification_message)
 
     except json.JSONDecodeError:
         return error_response(
